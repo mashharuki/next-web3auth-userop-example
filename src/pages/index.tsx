17644c48e69f263d796cfe4e3e7f99239e9bd6ea
mashharuki <avp.104.106.107@ae.auone-net.jp>
2023-08-05 11:46:08 +0900

    フォルダ構造を変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,27 @@
+import ActionButton from "@/components/ActionButton";
+import Button from "@/components/Button";
+import LoadingIndicator from "@/components/LoadingIndicator";
 import { CHAIN_NAMESPACES, SafeEventEmitterProvider } from "@web3auth/base";
 import { Web3Auth } from "@web3auth/modal";
 import {
-  getAddress,
   JsonRpcProvider,
+  Wallet,
+  getAddress,
   parseEther,
   toQuantity,
-  Wallet,
 } from "ethers";
 import { useEffect, useState } from "react";
 import { Client, Presets } from "userop";
+import {
+  entryPoint,
+  pmContext,
+  simpleAccountFactory
+} from "./../utils/constants";
 
-const entryPoint = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
-const simpleAccountFactory = "0x9406Cc6185a346906296840746125a0E44976454";
-const pmContext = {
-  type: "payg",
-};
+/**
+ * Home Component
+ * @returns 
+ */
 export default function Home() {
   const [web3auth, setWeb3auth] = useState<Web3Auth | null>(null);
   const [account, setAccount] = useState<Presets.Builder.SimpleAccount | null>(
@@ -47,23 +54,32 @@ export default function Home() {
   const paymaster = true
     ? Presets.Middleware.verifyingPaymaster(pmUrl, pmContext)
     : undefined;
+
+  /**
+   * 副作用フック
+   */
   useEffect(() => {
+    /**
+     * 初期化メソッド
+     */
     const init = async () => {
       setLoading(true);
       try {
-        const provider = new JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL);
+        const provider = new JsonRpcProvider(rpcUrl);
         const network = await provider.getNetwork();
         const chainId = network.chainId;
+        
+        // create Web3Auth object
         const web3auth = new Web3Auth({
           clientId: web3AuthClientId,
           web3AuthNetwork: "testnet",
           chainConfig: {
             chainNamespace: CHAIN_NAMESPACES.EIP155,
             chainId: toQuantity(chainId),
-            rpcTarget: process.env.NEXT_PUBLIC_RPC_URL,
+            rpcTarget: rpcUrl,
           },
         });
-
+        // initModal method
         await web3auth.initModal();
 
         setWeb3auth(web3auth);
@@ -78,7 +94,13 @@ export default function Home() {
     init();
   }, []);
 
+  /**
+   * createAccount method
+   * @param privateKey 
+   * @returns 
+   */
   const createAccount = async (privateKey: string) => {
+    // craete contract wallet
     return await Presets.Builder.SimpleAccount.init(
       new Wallet(privateKey) as any,
       rpcUrl,
@@ -88,25 +110,39 @@ export default function Home() {
     );
   };
 
+  /**
+   * get privatekey method
+   * @param provider 
+   * @returns 
+   */
   const getPrivateKey = async (provider: SafeEventEmitterProvider) => {
     return (await provider.request({
       method: "private_key",
     })) as string;
   };
 
+  /**
+   * setAuthorized method
+   * @param w3auth 
+   */
   const setAuthorized = async (w3auth: Web3Auth) => {
     if (!w3auth.provider) {
       throw new Error("web3authprovider not initialized yet");
     }
+    // authorized
     const authenticateUser = await w3auth.authenticateUser();
-
+    // get privateKey
     const privateKey = await getPrivateKey(w3auth.provider);
+    // crate contract wallet
     const acc = await createAccount(privateKey);
     setIdToken(authenticateUser.idToken);
     setAccount(acc);
     setPrivateKey(privateKey);
   };
 
+  /**
+   * login method
+   */
   const login = async () => {
     if (!web3auth) {
       throw new Error("web3auth not initialized yet");
@@ -119,6 +155,9 @@ export default function Home() {
     setAuthorized(web3auth);
   };
 
+  /**
+   * logout method
+   */
   const logout = async () => {
     if (!web3auth) {
       throw new Error("web3auth not initialized yet");
@@ -129,10 +168,19 @@ export default function Home() {
     setPrivateKey(null);
   };
 
+  /**
+   * add Event method
+   * @param newEvent 
+   */
   const addEvent = (newEvent: string) => {
     setEvents((prevEvents) => [...prevEvents, newEvent]);
   };
 
+  /**
+   * sendTransaction method
+   * @param recipient 
+   * @param amount 
+   */
   const sendTransaction = async (recipient: string, amount: string) => {
     setEvents([]);
     if (!account) {
@@ -144,6 +192,8 @@ export default function Home() {
 
     const target = getAddress(recipient);
     const value = parseEther(amount);
+    
+    // send UserOp
     const res = await client.sendUserOperation(
       account.execute(target, value, "0x"),
       {
@@ -160,107 +210,76 @@ export default function Home() {
     addEvent(`Transaction hash: ${ev?.transactionHash ?? null}`);
   };
 
-  if (loading) {
-    return <p>loading...</p>;
-  }
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <div></div>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          {idToken ? (
-            <div className="space-y-4">
-              <div className="flex justify-end space-x-4">
-                <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                  Logged in as&nbsp;
-                  <code className="font-mono font-bold text-green-300">
-                    {account?.getSender()}
-                  </code>
-                </p>
-
-                <button
-                  type="button"
-                  onClick={logout}
-                  className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 self-center"
-                >
-                  Logout
-                </button>
-              </div>
-              <div>
-                <div className="grid grid-cols-3 grid-rows-2 gap-4">
-                  <div className="col-span-1 row-span-2">
-                    <button
-                      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      onClick={() =>
-                        sendTransaction(
-                          "0x5DF100D986A370029Ae8F09Bb56b67DA1950548E",
-                          "0"
-                        )
-                      }
-                    >
-                      <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Transfer{" "}
-                      </h2>
-                      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Simple transfer of 0 ETH to an arbitrary address with
-                        gas sponsored.
-                      </p>
-                    </button>
-                    <button
-                      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      onClick={() =>
-                        privateKey
+        {loading ? (
+          <LoadingIndicator/>
+        ) : (
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            {idToken ? (
+              <div className="space-y-4">
+                <div className="flex justify-end space-x-4">
+                  <p className="flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                    Logged in as&nbsp;
+                    <code className="font-mono font-bold text-green-300">
+                      {account?.getSender()}
+                    </code>
+                  </p>
+                  {/* Logout Button */}
+                  <Button name="Logout" onClickFunction={logout} />
+                </div>
+                <div>
+                  <div className="grid grid-cols-3 grid-rows-2 gap-4">
+                    <div className="col-span-1 row-span-2">
+                      {/* Transfer Button */}
+                      <ActionButton
+                        name={"Transfer"}
+                        description={"Simple transfer of 0 ETH to an arbitrary address with gas sponsored."}
+                        onClickFunction={() =>
+                          sendTransaction(
+                            "0x5DF100D986A370029Ae8F09Bb56b67DA1950548E",
+                            "0"
+                          )}
+                      />
+                      {/* get Privatekey Button */}
+                      <ActionButton
+                        name={"Private Key"}
+                        description={"Print the private key of the account reconstructed by Web3Auth."}
+                        onClickFunction={() => privateKey
                           ? setEvents([`private key: ${privateKey}`])
                           : undefined
-                      }
-                    >
-                      <h2 className={`mb-3 text-2xl font-semibold`}>
-                        Private Key{" "}
-                      </h2>
-                      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Print the private key of the account reconstructed by
-                        Web3Auth.
-                      </p>
-                    </button>
-                    <button
-                      className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                      onClick={() =>
-                        idToken
-                          ? setEvents([`OAuth ID token: ${idToken}`])
-                          : undefined
-                      }
-                    >
-                      <h2 className={`mb-3 text-2xl font-semibold`}>
-                        OAuth ID Token{" "}
-                      </h2>
-                      <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        Print the OAuth ID Token. This token can be used to
-                        authenticate a user on the server.
-                      </p>
-                    </button>
-                  </div>
-                  <div className="overflow-scroll col-start-2 col-span-2 row-span-2 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    <div className="w-[1000px]">
-                      <div className="block whitespace-pre-wrap justify-center ">
-                        <pre>{events.join(`\n`)}</pre>
+                        }
+                      />
+                      {/* OAuth ID Token Button */}
+                      <ActionButton
+                        name={"OAuth ID Token"}
+                        description={"Print the OAuth ID Token. This token can be used to authenticate a user on the server."}
+                        onClickFunction={() =>
+                          idToken
+                            ? setEvents([`OAuth ID token: ${idToken}`])
+                            : undefined
+                        }
+                      />
+                    </div>
+                    <div className="overflow-scroll col-start-2 col-span-2 row-span-2 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                      <div className="w-[1000px]">
+                        <div className="block whitespace-pre-wrap justify-center ">
+                          <pre>{events.join(`\n`)}</pre>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <button
-              type="button"
-              onClick={login}
-              className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-            >
-              Login
-            </button>
-          )}
-        </div>
+            ) : (
+              <Button name="Login" onClickFunction={login} />
+            )}
+          </div>
+        )}
       </div>
     </main>
   );
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const entryPoint = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
+export const simpleAccountFactory = "0x9406Cc6185a346906296840746125a0E44976454";
+export const pmContext = {
+  type: "payg",
+};
